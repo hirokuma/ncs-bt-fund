d3980e15d7a3273295250e107a8f747db952b5fc
hirokuma <hiro9ma@gmail.com>
2024-08-18 10:12:14 +0900

    lesson2 exer2

--- lesson2/blefund_less2_exer2/src/main.c
+++ lesson2/blefund_less2_exer2/src/main.c
@@ -8,17 +8,29 @@
 #include <zephyr/logging/log.h>
 #include <zephyr/bluetooth/bluetooth.h>
 #include <zephyr/bluetooth/gap.h>
+#include <zephyr/usb/usb_device.h>
 #include <dk_buttons_and_leds.h>
 
 /* STEP 2.1 - Declare the Company identifier (Company ID) */
+#define COMPANY_ID_CODE            (0x0059)
 
 /* STEP 2.2 - Declare the structure for your custom data  */
+typedef struct adv_mfg_data {
+	uint16_t company_code;	    /* Company Identifier Code. */
+	uint16_t number_press;      /* Number of times Button 1 is pressed*/
+} adv_mfg_data_type;
 
 #define USER_BUTTON DK_BTN1_MSK
 
 /* STEP 1 - Create an LE Advertising Parameters variable */
+static const struct bt_le_adv_param *adv_param =
+	BT_LE_ADV_PARAM(BT_LE_ADV_OPT_NONE,
+	800,
+	801,
+	NULL);
 
 /* STEP 2.3 - Define and initialize a variable of type adv_mfg_data_type */
+static adv_mfg_data_type adv_mfg_data = {COMPANY_ID_CODE, 0x00};
 
 static unsigned char url_data[] = { 0x17, '/', '/', 'a', 'c', 'a', 'd', 'e', 'm',
 				    'y',  '.', 'n', 'o', 'r', 'd', 'i', 'c', 's',
@@ -35,20 +47,46 @@ static const struct bt_data ad[] = {
 	BT_DATA_BYTES(BT_DATA_FLAGS, BT_LE_AD_NO_BREDR),
 	BT_DATA(BT_DATA_NAME_COMPLETE, DEVICE_NAME, DEVICE_NAME_LEN),
 	/* STEP 3 - Include the Manufacturer Specific Data in the advertising packet. */
-
+	BT_DATA(BT_DATA_MANUFACTURER_DATA,(unsigned char *)&adv_mfg_data, sizeof(adv_mfg_data)),
 };
 
 static const struct bt_data sd[] = {
 	BT_DATA(BT_DATA_URI, url_data, sizeof(url_data)),
 };
 /* STEP 5 - Add the definition of callback function and update the advertising data dynamically */
+static void button_changed(uint32_t button_state, uint32_t has_changed)
+{
+	if (has_changed & button_state & USER_BUTTON) {
+		adv_mfg_data.number_press += 1;
+		bt_le_adv_update_data(ad, ARRAY_SIZE(ad),
+					sd, ARRAY_SIZE(sd));
+	}
+}
 
 /* STEP 4.1 - Define the initialization function of the buttons and setup interrupt.  */
+static int init_button(void)
+{
+	int err;
+
+	err = dk_buttons_init(button_changed);
+	if (err) {
+		printk("Cannot init buttons (err: %d)\n", err);
+	}
+
+	return err;
+}
 
 int main(void)
 {
 	int blink_status = 0;
 	int err;
+
+	if (IS_ENABLED(CONFIG_USB_DEVICE_STACK)) {
+		int ret = usb_enable(NULL);
+		if (ret) {
+			return -1;
+		}
+	}
 
 	LOG_INF("Starting Lesson 2 - Exercise 2 \n");
 
@@ -58,6 +96,11 @@ int main(void)
 		return -1;
 	}
 	/* STEP 4.2 - Setup buttons on your board  */
+	err = init_button();
+	if (err) {
+		printk("Button init failed (err %d)\n", err);
+		return -1;
+	}
 
 	err = bt_enable(NULL);
 	if (err) {
